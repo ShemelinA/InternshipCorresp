2b6b6b75d36c53cc3cde932ce7719ee7ed9b1af9
Alexey <sabaka_mail.ru@mail.ru>
2019-09-16 03:00:22 +0300

    add function mySortForKey

--- 2.php
+++ 2.php
@@ -1,29 +1,41 @@
 <?php
-/*
-
-convertString($a, $b). Результат ее выполнение: если в строке $a содержится 2 и более подстроки $b,
-то во втором месте заменить подстроку $b на инвертированную подстроку.
-
-mySortForKey($a, $b). $a – двумерный массив вида [['a'=>2,'b'=>1],['a'=>1,'b'=>3]], $b – ключ вложенного массива.
-Результат ее выполнения: двумерном массива $a отсортированный по возрастанию значений для ключа $b.
-В случае отсутствия ключа $b в одном из вложенных массивов, выбросить ошибку класса Exception с индексом неправильного массива.
-
-*/
 function convertString($a, $b)
 {
     $result = $a;
     $pos = strpos($a, $b);
-    if ($pos)
-    {
+    if ($pos) {
         $tempString = substr($a, $pos + 1);
         $pos2 = strpos($tempString, $b);
-        if ($pos2)
-        {
+        if ($pos2) {
             $part1 = substr($a, 0, $pos + 1);
-            $part2 = substr_replace($tempString, "===", $pos2, strlen($b));
+            $replacement = strrev(substr($tempString, $pos2, strlen($b)));
+            $part2 = substr_replace($tempString, $replacement, $pos2, strlen($b));
             $result = $part1 . $part2;
         }
     }
     return $result;
 }
-echo convertString("1234545456789", "454");
+
+function mySortForKey($a, $b)
+{
+    function build_sorter($key, $inpArray) {
+        return function ($a, $b) use ($key, $inpArray) {
+            try {
+                $existInA = array_key_exists($key, $a);
+                $existInB = array_key_exists($key, $b);
+                if ($existInA && $existInB) {
+                    return strnatcmp($a[$key], $b[$key]);
+                } else {
+                    $wrongArr = $existInA ? $b : $a;
+                    $i = array_search($wrongArr, $inpArray);
+                    throw new Exception('<br/>Неверный вложенный массив с индексом '.$i);
+                }
+            } catch (Exception $e) {
+                echo $e->getMessage();
+                return 0;
+            }
+        };
+    }
+    usort($a, build_sorter($b, $a));
+    return $a;
+}
